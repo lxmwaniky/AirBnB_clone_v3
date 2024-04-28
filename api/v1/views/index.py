5f8e26f7f8862abd9a149ec2c0982d5ba4d9f62b
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-28 17:26:30 +0300

    Refactor API views and remove unnecessary import in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """module - index file"""
 from models import storage
-from flask import jsonify, Blueprint
+from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
