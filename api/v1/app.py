74bbb6bcb93c9d07c1437c0db8cdcd63b722d481
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-28 17:29:48 +0300

    Refactor API views and update app configuration in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-""" Flask app for AirBnB clone """
-
-
+""" model - flask app """
 from os import environ
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,23 +12,23 @@ cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_database(error):
-    """ Teardown the app """
+def close_db(error):
+    """teardown app"""
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_not_found(error):
-    """ Handle 404 error """
+def not_found(error):
+    """ handling 404 error"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
-    """ Main function """
-    api_host = environ.get('HBNB_API_HOST')
-    api_port = environ.get('HBNB_API_PORT')
-    if not api_host:
-        api_host = '0.0.0.0'
-    if not api_port:
-        api_port = '5000'
-    app.run(host=api_host, port=api_port, threaded=True)
+    """main function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
