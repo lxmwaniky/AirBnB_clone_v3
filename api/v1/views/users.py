80d9ade6cc517ade851f78266dd8a0bf50694cda
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-29 14:48:24 +0300

    users

---
+++ api/v1/views/users.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+"""This module creates a users view"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+from datetime import datetime
+import uuid
+
+
+@app_views.route('/users/', methods=['GET'])
+@app_views.route('/users', methods=['GET'])
+def list_users():
+    list_users = [obj.to_dict() for obj in storage.all("User").values()]
+    return jsonify(list_users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
+        abort(404)
+    return jsonify(user_obj[0])
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
+        abort(404)
+    user_obj.remove(user_obj[0])
+    for obj in all_users:
+        if obj.id == user_id:
+            storage.delete(obj)
+            storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/users/', methods=['POST'])
+def create_user():
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'email' not in request.get_json():
+        abort(400, 'Missing name')
+    if 'password' not in request.get_json():
+        abort(400, 'Missing name')
+    users = []
+    new_user = User(email=request.json['email'],
+                    password=request.json['password'])
+    storage.new(new_user)
+    storage.save()
+    users.append(new_user.to_dict())
+    return jsonify(users[0]), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def updates_user(user_id):
+    """
+    Update a user based on the given user_id.
+
+    Args:
+        user_id (str): The ID of the user to be updated.
+
+    Returns:
+        tuple: A tuple containing the
+        updated user object and the HTTP status code.
+
+    Raises:
+        404: If the user with the given user_id is not found.
+        400: If the request data is not in JSON format.
+
+    """
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    try:
+        user_obj[0]['first_name'] = request.json['first_name']
+    except Exception:
+        pass
+    try:
+        user_obj[0]['last_name'] = request.json['last_name']
+    except Exception:
+        pass
+    for obj in all_users:
+        if obj.id == user_id:
+            try:
+                if request.json['first_name'] is not None:
+                    obj.first_name = request.json['first_name']
+            except Exception:
+                pass
+            try:
+                if request.json['last_name'] is not None:
+                    obj.last_name = request.json['last_name']
+            except Exception:
+                pass
+    storage.save()
+    return jsonify(user_obj[0]), 200
