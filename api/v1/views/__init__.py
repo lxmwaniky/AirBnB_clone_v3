df2e5ebe98fbbb8f3bd24f7ca95052d224295bed
carlarexy <carlanjeri17@gmail.com>
2024-05-01 06:23:15 -0700

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,9 @@
 """This module define a blueprint for routes with Blueprint object"""
 from flask import Blueprint
 
+
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,5 +13,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
